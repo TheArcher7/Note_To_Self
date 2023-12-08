edb4d8976dd228fa1d2ec366a4eb7e16c1bbb6e9
TheArcher7 <58994722+TheArcher7@users.noreply.github.com>
2023-12-08 15:14:07 -0700

    Chapter 16 updated the Note To Self app so that we can view notes in a recyclerView. I also removed two fragments and a navigator that were present by mistake because of the template chosen. The app functions, however, it does not persist data.

--- app/src/main/java/com/mmock/notetoself/MainActivity.kt
+++ app/src/main/java/com/mmock/notetoself/MainActivity.kt
@@ -1,29 +1,38 @@
 package com.mmock.notetoself
 
-// This is the code covered in Ch14.
-// There is some extra doe due to the templates being generated with additional fragments.
-// The textbook was a little bit outdated compared to Android Studio
+// This is the code covered in Ch16 expanding on code covered in ch14
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.widget.Button
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.mmock.notetoself.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     //temporary code
-    private var tempNote = Note()
+    //private var tempNote = Note()
+
+    private val noteList = ArrayList<Note>()
+    private var recyclerView: RecyclerView? = null
+    private var adapter: NoteAdapter? = null
+
     fun createNewNote(n: Note){
         //temporary code
-        tempNote = n
+        //tempNote = n
+        noteList.add(n)
+        adapter!!.notifyDataSetChanged()
+    }
+
+    fun showNote(noteToShow: Int) {
+        val dialog = DialogShowNote()
+        dialog.sendNoteSelected(noteList[noteToShow])
+        dialog.show(supportFragmentManager, "")
     }
 
     private lateinit var appBarConfiguration: AppBarConfiguration
@@ -34,28 +43,23 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
 
         binding.fab.setOnClickListener { view ->
             val dialog = DialogNewNote()
             dialog.show(supportFragmentManager, "")
         }
 
-        //temporary code
-        val button = findViewById<View>(R.id.button) as FloatingActionButton
-        button.setOnClickListener {
-            //create a new DialogShowNote
-            val dialog = DialogShowNote()
-            //send the note via the sendNoteSelected function
-            dialog.sendNoteSelected(tempNote)
-            //create the dialog
-            dialog.show(supportFragmentManager, "123")
-        }
+        recyclerView = findViewById<View>(R.id.recyclerView) as RecyclerView
+        adapter = NoteAdapter(this, noteList)
+        val layoutManager = LinearLayoutManager(applicationContext)
+        recyclerView!!.layoutManager = layoutManager
+        recyclerView!!.itemAnimator = DefaultItemAnimator()
+        //add a neat dividing line between items in the list
+        recyclerView!!.addItemDecoration(DividerItemDecoration(this, LinearLayoutManager.VERTICAL))
+        //set the adapter
+        recyclerView!!.adapter = adapter
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -74,9 +78,4 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
 }
